e17b2a51ce4480ccac07f11ef1f28d76061af592
Fabrizio Caldarelli <fabrizio.caldarelli@gmail.com>
2023-10-19 18:30:56 +0200

    Fix error: java.lang.SecurityException when registering a receiver - Android 14
    This is cherry-picked from a commit found in react-native-blob-util,
    a fork of rn-fetch-blob:
      https://github.com/RonRadtke/react-native-blob-util/commit/2554c32c1
    
    (cherry picked from commit 2554c32c1e628545af5051bee892389ddf4be243)

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
@@ -191,7 +191,11 @@ public class RNFetchBlobReq extends BroadcastReceiver implements Runnable {
                 DownloadManager dm = (DownloadManager) appCtx.getSystemService(Context.DOWNLOAD_SERVICE);
                 downloadManagerId = dm.enqueue(req);
                 androidDownloadManagerTaskTable.put(taskId, Long.valueOf(downloadManagerId));
-                appCtx.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+                if(Build.VERSION.SDK_INT >= 34 ){
+                    appCtx.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE), Context.RECEIVER_EXPORTED);
+                }else{
+                    appCtx.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+                }
                 return;
             }
 
